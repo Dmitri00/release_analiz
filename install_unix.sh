3837ab570873c594c08aaa77c14fc289b6aab997
Dmitri Toichkin <dima.toichkin@gmail.com>
2019-03-29 12:50:32 +0000

    corrected type nlkt.download(punKt)

--- install_unix.sh
+++ install_unix.sh
@@ -1,9 +1,17 @@
 pip3 install virtualenv
 
 virtualenv poisk_venv
+if [ $? -ne "0" ]
+then
+	python3 -m venv poisk_venv
+fi
 source poisk_venv/bin/activate
 pip3 install -r requirements.txt
-python -c 'import nltk;nltk.download("punct")'
+python -c 'import nltk;nltk.download("punkt")'
 
 virtualenv deactivate
+if [ $? -ne "0" ]
+then
+	deactivate
+fi
 python -c 'input("Установка выполнена успешно\nНажмите любую клавишу")'
